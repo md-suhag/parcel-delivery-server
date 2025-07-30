eeb65219d6d204be07f102ba831854dcc9c6e7a4
md-suhag <abdussalamsuhag104@gmail.com>
2025-07-30 14:47:46 +0600

    add: parcel and user interface defined

---
+++ src/app/modules/parcel/parcel.interface.ts
@@ -0,0 +1,53 @@
+import { Types } from "mongoose";
+
+export enum Status {
+  REQUESTED = "REQUESTED",
+  ASSIGNED = "ASSIGNED",
+  PICKED = "PICKED",
+  IN_TRANSIT = "IN_TRANSIT",
+  DELIVERED = "DELIVERED",
+  CANCELLED = "CANCELLED",
+  RETURNED = "RETURNED",
+}
+export enum Division {
+  DHAKA = "DHAKA",
+  CHITTAGONG = "CHITTAGONG",
+  RAJSHAHI = "RAJSHAHI",
+  KHULNA = "KHULNA",
+  BARISAL = "BARISAL",
+  SYLHET = "SYLHET",
+  RANGPUR = "RANGPUR",
+  MYMENSINGH = "MYMENSINGH",
+}
+
+interface IReceiver {
+  name: string;
+  phone: string;
+  email?: string;
+  address: string;
+  city: Division;
+}
+
+interface IStatusLog {
+  location?: string;
+  time: Date;
+  status: Status;
+  note?: string;
+}
+
+export interface IParcel {
+  trackingId: string;
+  type: string;
+  weight: string;
+  deliveryFee: number;
+  sender: Types.ObjectId;
+  receiver: IReceiver;
+  assignedRider?: Types.ObjectId;
+  pickingAddress: string;
+  deliveryDate: Date;
+  status: Status;
+  statusLogs: IStatusLog[];
+  isBlocked?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,30 @@
+export enum Role {
+  ADMIN = "ADMIN",
+  SENDER = "SENDER",
+  RECEIVER = "RECEIVER",
+  RIDER = "RIDER",
+}
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+export interface IAuthProvider {
+  provider: "google" | "credentials";
+  providerId: string;
+}
+
+export interface IUser {
+  name: string;
+  phone: string;
+  email: string;
+  password?: string;
+  address: string;
+  role: Role;
+  auths: IAuthProvider;
+  isDeleted?: boolean;
+  isActive?: IsActive;
+  isVerified?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
